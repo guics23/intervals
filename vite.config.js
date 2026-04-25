211b79ec945a9ff6297a267a8364040d2993f0f9
Guillem Comes <gcomes@debelop.com>
2026-04-25 20:44:00 +0200

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/intervals/',
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.svg', 'icon-192.png', 'icon-512.png', 'sounds/*.mp3'],
+      manifest: {
+        name: 'In Key',
+        short_name: 'In Key',
+        description: 'Ear training and degree identification.',
+        theme_color: '#111111',
+        background_color: '#111111',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/intervals/',
+        start_url: '/intervals/',
+        icons: [
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,mp3,png,svg,webmanifest}'],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      },
+    }),
+  ],
 })
